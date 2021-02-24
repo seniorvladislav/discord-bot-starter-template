f6d962002aad3132f7f6f398f0a338e7f35ba0ef
seniorvladislav <vmelikyan161@gmail.com>
2021-02-24 11:26:32 +0300

    The End of the Caturday (22.02.2021)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const { Client } = require("discord.js");
+const needle = require("needle");
+const trackMemberLog = require("./utils/trackMemberLog");
 
 const client = new Client();
 
-const { token, prefix } = require("./config.json");
+const { token, prefix, THE_CAT_API_KEY } = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Успешное подключение к боту`);
   console.log(`Меня зовут ${client.user.username}\n\n`);
+  trackMemberLog(client);
 });
 
 client.on("message", (msg) => {
@@ -15,6 +18,44 @@ client.on("message", (msg) => {
   if (msg.author.bot || !msg.content.startsWith(prefix)) return;
 
   const command = msg.content.toLowerCase().slice(prefix.length);
+
+  if (command === "дата") {
+    const date = new Date().toLocaleDateString();
+    msg.channel.send(`Сейчас на сервере: ${date}`);
+  } else if (command === "время") {
+    const time = new Date().toLocaleTimeString();
+    msg.channel.send(`Время на сервере: ${time}`);
+  } else if (command === "датавремя") {
+    const date = new Date().toLocaleString();
+    msg.channel.send(date);
+  } else if (command.match(/ава|avatar/)) {
+    const user = msg.mentions.users.first() || msg.author;
+
+    const avatarURL = user.displayAvatarURL({
+      size: 1024,
+      dynamic: true,
+      format: "png",
+    });
+
+    msg.channel.send(avatarURL);
+  } else if (command.match(/коты?|cats?/)) {
+    // } else if (command.startsWith('кот') || command.startsWith('cat')) {
+    const endpoint = "https://api.thecatapi.com/v1/images/search";
+
+    needle("get", endpoint, {
+      headers: {
+        "x-api-key": THE_CAT_API_KEY,
+      },
+    })
+      .then((response) => {
+        const { body } = response;
+        // console.log(body);
+        const cat = body[0];
+
+        msg.channel.send(cat.url);
+      })
+      .catch((err) => console.error(err));
+  }
 });
 
 client.login(token);
